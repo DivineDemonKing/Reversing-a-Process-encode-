cc27c9e49a803822fd0873a8845b7d3e2da16ce6
Георгий <@DESKTOP-IV0D95M>
2022-11-18 13:48:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите послание");
+string thingToEncrypt = Console.ReadLine();
+Console.WriteLine("Введите ключ");
+string temp = Console.ReadLine();
+int key;
+int.TryParse(temp, out key);
+List<char> letters = new List<char>();
+int temp1;
+for (int i = 0; i < thingToEncrypt.Length; i++)
+{
+    letters.Add((char)((thingToEncrypt[i]) % 97 * key % 26 + 97));
+}
+Console.Write(key);
+foreach (char letter in letters)
+{
+    Console.Write(letter);
+}
